33fce71ca184164ea47480b7c5cbb3ea44ab3873
JuanmaSalva <jsalva@ucm.es>
2022-01-22 13:39:48 +0100

    Cambio a maven project

--- Eclipse/ComunicacionJavaArduino/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module es.ucm.fdi.tfg.robotpacman.pruebas {
-}
--- Eclipse/src/main/java/es/ucm/fdi/tfg/robotpacman/pruebas/main.java
+++ Eclipse/src/main/java/es/ucm/fdi/tfg/robotpacman/pruebas/main.java
@@ -1,10 +1,10 @@
 package es.ucm.fdi.tfg.robotpacman.pruebas;
 
-public class HelloWorld {
+public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hellos world maven");
 	}
 
 }
