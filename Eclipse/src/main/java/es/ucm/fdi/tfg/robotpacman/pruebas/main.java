7a4a7dbd634bbf28ec326269bacc360f5076df54
JuanmaSalva <jsalva@ucm.es>
2022-02-03 10:28:47 +0100

    Simulador subido y funcional

--- Eclipse/src/main/java/es/ucm/fdi/tfg/robotpacman/pruebas/main.java
+++ Eclipse/src/main/java/es/ucm/fdi/tfg/robotpacman/pruebas/main.java
@@ -19,6 +19,7 @@ public class main {
 		if(sp.openPort()) {
 			System.out.println("Puerto abierto");
 			sp.getInputStream().close();
+			
 			//Thread.sleep(2000); //Tiempo para que se inicialice el arduino
 		}
 		else {
